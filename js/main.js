3ae64d716ace7e44945f4213e1e3171fd6a4e013
lilitantonyan <lilit@helix.am>
2024-12-06 17:56:04 +0400

    add files

--- js/main.js
+++ js/main.js
@@ -5,5 +5,23 @@ $(function (){
         $(this).addClass('dropdown-item-selected')
         $('body').removeClass('dark');
         $('body').addClass($(this).data('name'));
+    });
+
+    $('.notes-folders .note-link').on('click',function (e){
+        e.preventDefault();
+        $('.notes-folders  .note-item').removeClass('selected');
+        $(this).parent().addClass('selected');
+        $('.aside-opened').addClass('opened');
+        $('aside:not(.aside-opened)').addClass('opened').removeClass('show');
+        $('main').addClass('main-opened').removeClass('main-folder');
+
+    })
+    $('.btn-back').on('click',function (){
+        $('.notes-folders  .note-item').removeClass('selected');
+        $('.aside-opened').removeClass('opened');
+        $('.aside-opened').hide();
+        $('main').removeClass('main-opened');
+        $('aside:not(.aside-opened)').removeClass('opened').addClass('show');
+
     })
 })
